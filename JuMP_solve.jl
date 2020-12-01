13b19f686d1550444ef6fd51fb3832051f4aac1a
Dylan Clarke <34451773+dylanomics@users.noreply.github.com>
2020-12-01 13:31:21 -0500

    Create jump_solve.jl

---
+++ JuMP_solve.jl
@@ -0,0 +1,31 @@
+using JuMP, Ipopt
+
+# cost vector
+c = [3, 5, 7, 3, 2, 5]
+N = size(c)
+
+# constraint matrices
+A = [1 1 1 0 0 0
+     0 0 0 1 1 1
+     1 0 0 1 0 0
+     0 1 0 0 1 0
+     0 0 1 0 0 1]
+b = [ 25,  35,  20,  30,  10]
+s = ['<', '<', '=', '=', '=']
+
+# construct model
+model = Model(Ipopt.Optimizer)
+@variable(model, x[i=1:N] >= 0, base_name="traded quantities")
+cost_fn = @expression(model, c'*x)                                              # cost function
+@constraint(model, C1, A[1:2,:]*x .<= b[1:2])                                   # inequality constraints
+@constraint(model, C2, A[3:5,:]*x .== b[3:5])                                   # equality constraints
+@objective(model, Min, cost_fn)                                                 # objective function
+
+# solve model
+status = JuMP.optimize!(model);
+xstar = value.(x); xstar
+println("total cost = ", JuMP.objective_value(model))
+
+# recover Lagrange multipliers for post-optimality
+λ = JuMP.dual(C1[1]),JuMP.dual(C1[2])
+μ = JuMP.dual(C2[1]),JuMP.dual(C2[2]),JuMP.dual(C2[3])
